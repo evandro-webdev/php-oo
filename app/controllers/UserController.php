01e2f4f4b0be9d03295823d433e4b34d6d785701
Evandro Mateus <evandromateus06@gmail.com>
2024-05-09 12:25:36 -0300

    implementanto request only e except

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -2,10 +2,16 @@
 
 namespace app\controllers;
 
+use app\core\Request;
+
 class UserController extends Controller
 {
   public function edit($params)
   {
-    $this->view('user', ['title' => 'Página do usuário', 'name' => 'Evandro']);
+    $this->view('user', ['title' => 'Editar usuário']);
+  }
+
+  public function update($params)
+  {
   }
 }
--- app/views/user.php
+++ app/views/user.php
@@ -1,8 +1,12 @@
 <?php $this->layout('master', ['title' => $title]) ?>
 
-<?php $this->start('css') ?>
-<link rel="stylesheet" href="/css/styles.css">
-<?php $this->stop() ?>
-
 <h1>User</h1>
-<p>Hello, <?php echo $this->e($name) ?></p>
+
+<form action="/user/update/12" method="post">
+  <input type="text" name="firstName" value="Evandro">
+  <input type="text" name="lastName" value="Mateus">
+  <input type="text" name="email" value="evandro@gmail.com">
+  <input type="password" name="password" value="12345">
+
+  <button type="submit">Atualizar</button>
+</form>

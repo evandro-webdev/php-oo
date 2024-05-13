10329b2f9443eef3d111e4780383126fcca53c97
Evandro Mateus <evandromateus06@gmail.com>
2024-05-13 16:37:46 -0300

    adicionando filtros de busca

---
+++ app/database/Filters.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\database;
+
+class Filters
+{
+  private array $filters = [];
+
+  public function where(string $field, string $operator, mixed $value, string $logic = '')
+  {
+    $formatter = '';
+
+    if (is_array($value)) {
+      $formatter = "('" . implode("','", $value) . "')";
+    } else if (is_string($value)) {
+      $formatter = "'{$value}'";
+    } else if (is_bool($value)) {
+      $formatter = $value ? 1 : 0;
+    } else {
+      $formatter = $value;
+    }
+
+    $value = strip_tags($formatter);
+
+    $this->filters['where'][] = "{$field} {$operator} {$value} {$logic}";
+  }
+
+  public function orderBy(string $field, string $order = 'asc')
+  {
+    $this->filters['order'] = "ORDER BY {$field} {$order} ";
+  }
+
+  public function limit(int $limit)
+  {
+    $this->filters['limit'] = "LIMIT {$limit}";
+  }
+
+  public function dump()
+  {
+    $filter = !empty($this->filters['where']) ? 'WHERE ' . implode(' ', $this->filters['where']) : '';
+    $filter .= $this->filters["order"] ?? '';
+    $filter .= $this->filters["limit"] ?? '';
+
+    return $filter;
+  }
+}
